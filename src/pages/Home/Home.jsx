f1238091937641fc1918c05ef9e130bf46607f5e
Md Ataullah <ataullahm100@gmail.com>
2024-05-17 09:40:48 +0600

    Header subheader added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from '../../components/Banner/Banner';
+import Heading from '../../components/Heading/Heading';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Heading text='---From 11:00am to 10:00pm---' hed='ORDER ONLINE' />
     </div>
   );
 };
