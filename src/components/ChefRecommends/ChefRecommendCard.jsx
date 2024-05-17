5016422a1ea15fcf92cf287fe1a4b7f6d14b1ea2
Md Ataullah <ataullahm100@gmail.com>
2024-05-17 12:07:01 +0600

    Footer added

--- src/components/ChefRecommends/ChefRecommendCard.jsx
+++ src/components/ChefRecommends/ChefRecommendCard.jsx
@@ -8,14 +8,14 @@ const ChefRecommendCard = () => {
           backgroundImage: 'url(' + `${img1}` + ')',
         }}
       ></div>
-      <div className="py-5">
+      <div className="py-5 flex flex-col items-center justify-center">
         <h3 className="text-center text-neutral-900 text-2xl font-semibold font-['Inter']">
           Caeser Salad
         </h3>
         <p className="max-w-[345px] mx-auto px-3 text-center text-neutral-900 text-base font-normal font-['Inter'] leading-relaxed">
           Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.
         </p>
-        <button className="w-[196px] mx-auto h-14 bg-gray-200 hover:bg-gray-800 duration-300 rounded-lg border-b-2 border-yellow-600 hover:border-gray-800 mt-4 flex justify-center items-center text-center text-yellow-600 text-xl font-medium font-['Inter'] uppercase">
+        <button className="py-2 px-5 bg-gray-200 hover:bg-gray-800 duration-300 rounded-lg border-b-2 border-yellow-600 hover:border-gray-800 mt-4 flex justify-center items-center text-center text-yellow-600 text-lg font-medium font-['Inter'] uppercase">
           add to cart
         </button>
       </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,42 @@
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+
 const Footer = () => {
   return (
-    <footer className="p-4 bg-neutral-900 text-white mt-10">
-      <p>
+    <footer className="bg-neutral-900 text-white mt-10">
+      <div className="flex flex-col md:flex-row items-center ">
+        <div className="w-full md:w-1/2 bg-gray-800 py-20 text-right md:pr-10 lg:pr-20">
+          <div className=" max-w-96 text-center flex flex-col justify-center items-center ml-auto">
+            <h3 className="text-white text-[32px] font-medium font-['Inter']">
+              CONTACT US
+            </h3>
+            <div className="text-white text-xl font-medium font-['Inter'] leading-9">
+              123 ABS Street, Uni 21, Bangladesh
+              <br />
+              +88 123456789
+              <br />
+              Mon - Fri: 08:00 - 22:00
+              <br />
+              Sat - Sun: 10:00 - 23:00
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 bg-gray-900 py-20 md:pl-10 lg:pl-20">
+          <div className="max-w-80 text-center flex flex-col justify-center items-center">
+            <h2 className="text-white text-[32px] font-medium font-['Inter']">
+              Follow US
+            </h2>
+            <p className="text-white text-xl font-medium font-['Inter'] leading-9">
+              Join us on social media
+            </p>
+            <div className="flex items-center gap-3 text-2xl pt-6">
+              <FaFacebookF />
+              <FaInstagram />
+              <FaTwitter />
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-center py-4">
         Copyright © 2024 - All right reserved by{' '}
         <a
           href="https://www.facebook.com/ataullah0"
