6606e320d9b83af3aec8cb5ee614831de6981cd7
Md Ataullah <ataullahm100@gmail.com>
2024-05-17 10:08:14 +0600

    Middle banner added "\ g

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import Banner from '../../components/Banner/Banner';
+import CategorisSlider from '../../components/CategorisSlider/CategorisSlider';
 import Heading from '../../components/Heading/Heading';
+import MiddleBanner from '../../components/MiddleBanner/MiddleBanner';
 
 const Home = () => {
   return (
     <div>
       <Banner />
-      <Heading text='---From 11:00am to 10:00pm---' hed='ORDER ONLINE' />
+      <div className="w-10/12 mx-auto">
+        <Heading text="---From 11:00am to 10:00pm---" hed="ORDER ONLINE" />
+        <CategorisSlider />
+        <MiddleBanner />
+      </div>
     </div>
   );
 };
