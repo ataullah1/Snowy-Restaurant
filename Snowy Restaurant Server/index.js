1c078d116336c79ab42cf24bf73a22b020d673c9
Md Ataullah <ataullahm100@gmail.com>
2024-05-21 13:58:07 +0600

    CARD DELETE METHON ADDED

--- Snowy Restaurant Server/index.js
+++ Snowy Restaurant Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
@@ -46,10 +46,24 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
+    // Cart data delete
+    app.delete('/carts-delete:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+    // // My Cart data get
+    // app.get('/my-carts', async (req, res) => {
+    //   const email = req.query.email;
+    //   const query = { email: email };
+    //   const result = await cartCollection.find(query).toArray();
+    //   res.send(result);
+    // });
     // Cart adding method
     app.post('/carts', async (req, res) => {
       const cartItem = req.body;
-      console.log(cartItem);
+      // console.log(cartItem);
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
